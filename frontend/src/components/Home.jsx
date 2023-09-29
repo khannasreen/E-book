4a6ed64a850b0fe30a31803d1dad4261c9b4f613
khannasreen <nasreenkhan61926@gmail.com>
2023-09-29 19:31:50 +0530

    My Project

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -21,15 +21,15 @@ const Home = () => {
   }, []);
 
   const displayData = () => {
-    return useData.map((laptop) => (
+    return useData.map((user) => (
        <div className="col-md-3 mb-4">
             <div className="card">
-                <img className="card-img-top" src={laptop.image} alt="" />
+                <img className="card-img-top" src={user.image} alt="" />
                 <div className="card-body">
-                    <h4>{laptop.volumeInfo.title}</h4>
-                    <h3>{laptop.volumeInfo.subtitle}</h3>
-                    <h2>{laptop.publishedDate}</h2>
-                    <h6>{laptop.volumeInfo.description}</h6>
+                    <h4>{user.volumeInfo.title}</h4>
+                    <h3>{user.volumeInfo.subtitle}</h3>
+                    <h2>{user.publishedDate}</h2>
+                    <h6>{user.volumeInfo.description}</h6>
                     
                 </div>
             </div>
@@ -75,99 +75,3 @@ const Home = () => {
 
 export default Home
 
-// sir code
-// import React, { useEffect, useState } from "react"
-// import toast from "react-hot-toast";
-// import { Link } from "react-router-dom";
-
-
-
-// const ManageUser = () => {
-//     const [useData, setUserData] = useState([]);
-
-
-//     const fetchUserData = async () => {
-//         const res = await fetch('http://localhost:5000/user/getall');
-//         console.log(res.status);
-
-//         if (res.status === 200) {
-//             const data = await res.json();
-//             console.log(data);
-//             setUserData(data);
-//         }
-//     };
-//     useEffect(() => {
-//         fetchUserData()
-//     }, []);
-
-
-//     const deleteUser = async (id) => {
-//         toast.promise(
-//             fetch("http://localhost:5000/user/delete/" + id, { method: "DELETE" }),
-//             {
-//                 loading: "Deleting...",
-//                 success: () => {
-//                     fetchUserData();
-//                     return <b>User Deleted!</b>;
-//                 },
-//                 error: <b>Could not delete.</b>,
-//             }
-//         );
-//     };
-
-
-//     return (
-//         <div>
-//             <div className='container py-5'>
-//                 <h3 className='text-center'>This is manage user</h3>
-//                 <table className="table">
-//                     <thead>
-//                         <tr>
-//                             <th>Id</th>
-//                             <th>Name</th>
-//                             <th>Father Name</th>
-//                             <th>Email</th>
-//                             <th>Password</th>
-//                             <th>Action</th>
-//                             <th>View</th>
-//                         </tr>
-//                     </thead>
-//                     <tbody>
-//                         {
-//                             useData.map((user) => {
-//                                 return <tr>
-//                                     <td>{user._id}</td>
-//                                     <td>{user.name}</td>
-//                                     <td>{user.fathername}</td>
-//                                     <td>{user.email}</td>
-//                                     <td>{user.password}</td>
-//                                     <td><button
-//                                         onClick={() => {
-//                                             deleteUser(user._id);
-//                                         }}
-//                                         className="btn btn-danger"
-//                                     >
-//                                         Delete</button></td>
-
-//                                     <td>
-                        
-//                                         <Link to={'/updateuser/' +user._id} className="btn btn-primary">
-//                                             View User
-//                                         </Link>
-//                                     </td>
-//                                 </tr>
-
-//                             })
-//                         }
-
-
-//                     </tbody>
-//                 </table>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default ManageUser
-
-// sir code end
